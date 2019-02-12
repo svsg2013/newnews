ba03ddf9866b158b85ac9dc3d8d2f7c0d5d78b71
Thanh Tran <lethanhtdt@gmail.com>
2019-02-12 17:16:58 +0700

    update code

--- resources/views/newnews/blogSingle.blade.php
+++ resources/views/newnews/blogSingle.blade.php
@@ -168,7 +168,7 @@
                         </div><!-- Box info -->
                         <!-- san pham lien quan -->
                         <div class="col_full nobottommargin clearfix">
-                            <div class="line"></div>
+                            <div class="lineCustom"></div>
                             <div class="fancy-title title-border">
                                 <h4>Sản phẩm liên quan</h4>
                             </div>
@@ -181,9 +181,8 @@
                                     <div class="entry-title">
                                         <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
                                     </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 9th Sep 2014</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 23</a></li>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
                                     </ul>
                                     <div class="entry-content">
                                         <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
@@ -194,52 +193,13 @@
                             <div class="col_one_third">
                                 <div class="ipost clearfix">
                                     <div class="entry-image">
-                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/16.jpg" alt="Image"></a>
+                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/11.jpg" alt="Image"></a>
                                     </div>
                                     <div class="entry-title">
-                                        <h3><a href="blog-single.html">Halliburton gets boost from rebound in North America drilling</a></h3>
+                                        <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
                                     </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 23rd Aug 2014</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 33</a></li>
-                                    </ul>
-                                    <div class="entry-content">
-                                        <p>Eaque iusto quod assumenda beatae, nesciunt aliquid. Vel, eos eligendi?</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col_one_third col_last">
-                                <div class="ipost clearfix">
-                                    <div class="entry-image">
-                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/13.jpg" alt="Image"></a>
-                                    </div>
-                                    <div class="entry-title">
-                                        <h3><a href="blog-single.html">China sends spy ship off Hawaii during U.S.-led drills brews</a></h3>
-                                    </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 11th Feb 2014</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
-                                    </ul>
-                                    <div class="entry-content">
-                                        <p>Magni impedit quae consectetur consequuntur adipisci veritatis modi a, officia cum.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="clear"></div>
-
-                            <div class="col_one_third nobottommargin">
-                                <div class="ipost clearfix">
-                                    <div class="entry-image">
-                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/10.jpg" alt="Image"></a>
-                                    </div>
-                                    <div class="entry-title">
-                                        <h3><a href="blog-single.html">Wobbly stocks underpin yen and Swiss franc; dollar subdued</a></h3>
-                                    </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 17th Jan 2014</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 50</a></li>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
                                     </ul>
                                     <div class="entry-content">
                                         <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
@@ -247,45 +207,79 @@
                                 </div>
                             </div>
 
-                            <div class="col_one_third nobottommargin">
+                            <div class="col_one_third col_last">
                                 <div class="ipost clearfix">
                                     <div class="entry-image">
-                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/15.jpg" alt="Image"></a>
+                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/11.jpg" alt="Image"></a>
                                     </div>
                                     <div class="entry-title">
-                                        <h3><a href="blog-single.html">BlackBerry names ex-Sybase executive as chief operating officer</a></h3>
+                                        <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
                                     </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 20th Nov 2014</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
                                     </ul>
                                     <div class="entry-content">
-                                        <p>Eaque iusto quod assumenda beatae, nesciunt aliquid. Vel, eos eligendi?</p>
+                                        <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col_one_third nobottommargin col_last">
+                            <div class="clear"></div>
+
+                            <div class="col_one_third">
                                 <div class="ipost clearfix">
                                     <div class="entry-image">
-                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/6.jpg" alt="Image"></a>
+                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/11.jpg" alt="Image"></a>
                                     </div>
                                     <div class="entry-title">
-                                        <h3><a href="blog-single.html">Georgian prime minister fires seven ministers in first reshuffle</a></h3>
+                                        <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
                                     </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> 10th Dec 2013</li>
-                                        <li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
                                     </ul>
                                     <div class="entry-content">
-                                        <p>Magni impedit quae consectetur consequuntur adipisci veritatis modi a, officia cum.</p>
+                                        <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col_one_third">
+                                <div class="ipost clearfix">
+                                    <div class="entry-image">
+                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/11.jpg" alt="Image"></a>
+                                    </div>
+                                    <div class="entry-title">
+                                        <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
+                                    </div>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
+                                    </ul>
+                                    <div class="entry-content">
+                                        <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col_one_third col_last">
+                                <div class="ipost clearfix">
+                                    <div class="entry-image">
+                                        <a href="#"><img class="image_fade" src="images/magazine/thumb/11.jpg" alt="Image"></a>
+                                    </div>
+                                    <div class="entry-title">
+                                        <h3><a href="blog-single.html">Yum, McDonald's apologize as new China food scandal brews</a></h3>
+                                    </div>
+                                    <ul class="entry-meta prices clearfix">
+                                        <li>Đơn giá: 2.0000.000 vnđ</li>
+                                    </ul>
+                                    <div class="entry-content">
+                                        <p>Neque nesciunt molestias soluta esse debitis. Magni impedit quae consectetur consequuntur.</p>
                                     </div>
                                 </div>
                             </div>
 
                         </div>
                         <!-- end san pham lien quan -->
-                        <div class="line"></div>
+                        <div class="line lineCustom"></div>
 
                         <h4>Tin Liên Quan</h4>
 
